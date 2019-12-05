073fd5e5baaf7b2dfbda09b985637486cf0ba36c
antoineegret <a.egret@laposte.net>
2019-12-05 14:36:43 +0100

    exercie 4

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exo4</title>
+</head>
+<body>
+    <?php
+    $txt = "chuck norris ne croit pas en dieu, mais dieu croit en chuck norris";
+    $int = "56";
+    $float = 10.365;
+    echo $txt;
+    ?>
+    <br>
+    <?php echo $int; ?>
+    <br>
+    <?php echo $float; ?>
+</body>
+</html>
